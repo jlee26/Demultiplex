0235a680bdfdfe8a8118cc364e7b4a35e980ac59
jlee26 <38050578+jlee26@users.noreply.github.com>
2021-08-01 21:27:20 -0700

    Add files via upload

---
+++ Assignment-the-first/demultiplex_r2.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import numpy as np
+import Bioinfo
+import gzip
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+# file_r1 = "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R1_001.fastq.gz"
+file_r2 = "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R2_001.fastq.gz"
+# file_r3 = "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R3_001.fastq.gz"
+# file_r4 = "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R4_001.fastq.gz"
+# dict_files = {
+#     "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R1_001.fastq.gz": ["read 1",1],
+#     "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R2_001.fastq.gz": ["index 1",2],
+#     "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R3_001.fastq.gz": ["index 2",3],
+#     "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R4_001.fastq.gz": ["read 2",4]}
+
+# tfile_r1 = "./unit_test_r1.fastq.gz"
+# tfile_r2 = "./unit_test_r2.fastq.gz"
+# tfile_r3 = "./unit_test_r3.fastq.gz"
+# tfile_r4 = "./unit_test_r4.fastq.gz"
+# dict_files = {
+#     "./unit_test_r1.fastq.gz": ["read 1",1],
+#     "./unit_test_r2.fastq.gz": ["index 1",2],
+#     "./unit_test_r3.fastq.gz": ["index 2",3],
+#     "./unit_test_r4.fastq.gz": ["read 2",4]}
+
+
+# for file_name, ri in dict_files.items():
+    ### Reading in file and creating an array of the quality scores at each position for all reads to calculate the average
+dict_tot = {}
+with gzip.open(file_r2, "rb") as fr1:
+    num_seq = 0
+    count = 0
+    for line in fr1:
+        line = line[:-1]
+        if count % 4 == 3:
+            num_seq += 1
+            seq_len = len(line)
+            tot = 0
+            for i in range(len(line)):
+                if i in dict_tot.keys():
+                    dict_tot[i] += line[i] - 33
+                else:
+                    dict_tot[i] = line[i] - 33
+        count += 1
+
+dict_ave = {}    
+for base, tot in dict_tot.items():
+    ave = tot/num_seq
+    dict_ave[base] = ave
+
+print("Number of records: ", num_seq) #number of lines
+print("Number of bases: ",seq_len) #legth of the sequence
+print("Total number of lines: ",count)
+
+x = range(len(dict_ave))
+plt.figure()
+plt.bar(x, dict_ave.values())
+plt.title("The average quality scores for each position for Index 1")
+plt.xlabel("Position")
+plt.ylabel("Mean")
+plt.savefig("index1.png")
---
+++ Assignment-the-first/wrapper_demultiplex_r2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition=bgmp        ### Partition
+#SBATCH --job-name=demult_r2_%j      ### Job Name
+#SBATCH --output=demult_r2_%j.out         ### File in which to store job output
+#SBATCH --error=demult_r2_%j.err          ### File in which to store job error messages
+#SBATCH --time=1-00:00:00       ### Wall clock time limit in Days-HH:MM:SS
+#SBATCH --nodes=1               ### Number of nodes needed for the job
+#SBATCH --ntasks-per-node=8     ### Number of tasks to be launched per Node
+#SBATCH --account=bgmp      ### Account used for job submission
+#SBATCH --mail-user='jlee26@uoregon.edu'
+#SBATCH --mail-type=END,FAIL
+export OMP_NUM_THREADS=8
+conda activate bgmp_py39
+
+./demultiplex_r2.py
